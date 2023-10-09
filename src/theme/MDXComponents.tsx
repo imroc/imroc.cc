9e575c120085638fcb3143138da0129749a80604
roc <roc@imroc.cc>
2023-10-09 14:40:43 +0800

    update at 2023-10-09 14:40:43

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 // Import the original mapper
 import MDXComponents from '@theme-original/MDXComponents';
 import FileBlock from '@site/src/components/FileBlock';
+import Tabs from '@theme-original/Tabs';
+import TabItem from '@theme-original/TabItem';
+import CodeBlock from '@theme-original/CodeBlock';
 
 export default {
   // Re-use the default mapping
   ...MDXComponents,
+  CodeBlock,
   FileBlock,
+  Tabs,
+  TabItem,
 };
